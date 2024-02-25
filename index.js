375e5992a38d63dae09c9fa56d1faaced6d32cd6
saurabh sagar <saurabh.sagar564@gmail.com>
2024-02-25 20:44:49 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,151 @@
+const puppeteer = require('puppeteer');
+const cheerio = require('cheerio');
+const { createObjectCsvWriter } = require('csv-writer');
+const fs = require('fs');
+const csv = require('csv-parser');
+
+
+function combinedArrays(arr1, arr2) {
+    if (arr1.length !== arr2.length) {
+        throw new Error("Not Equal length");
+    }
+    
+    const newarray = [];
+    
+    for (let i = 0; i < arr1.length; i++) {
+        const mergedObject = { ...arr1[i], ...arr2[i] };
+        newarray.push(mergedObject);
+    }
+    
+    return newarray;
+}
+
+
+async function scrape(companyList) {
+  const comanyLinkedinUrl = []
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+  await page.setViewport({ width: 1200, height: 800 });
+  await page.goto('https://www.linkedin.com/login', { waitUntil: 'domcontentloaded' });
+
+  const isLoginPage = await page.$('#username') && await page.$('#password');
+
+  if (isLoginPage) {
+    await page.type('#username', 'sgrrsaurabh564@gmail.com');
+    await page.type('#password', 'Saumya99@');
+    await page.click('.login__form_action_container button');
+  
+    await page.waitForNavigation();
+  }
+
+  const csvWriter = createObjectCsvWriter({
+    path: 'results.csv',
+    header: [
+      { id: 'company', title: 'companyName' },
+      { id: 'linkedin_url', title: 'linkedinPageurl' },
+      { id: 'people_name', title: 'PeopleName' },
+      { id: 'role', title: 'Role' },
+      { id: 'website', title: 'Website' },
+      { id: 'phone', title: 'Phone' }
+    ],
+  });
+
+  const companyData = [];
+  for (const company of companyList) {
+  await page.goto(`https://www.linkedin.com/search/results/companies/?keywords=${encodeURIComponent(company)}`, { waitUntil: 'domcontentloaded' });
+  const currentUrl = page.url();
+  console.log(`URL after searching for ${company}:`, currentUrl);
+
+  const searchResultHTML = await page.content();
+  const $ = cheerio.load(searchResultHTML);
+
+  const results = $('.scaffold-layout__main');
+
+  if (results.length > 0) {
+    results.each((index, element) => {
+      const companyName = $(element).find('.entity-result__title-text').text().trim();
+      const company = companyName.replace(/\s+/g, ' ').trim();
+      const companyUrl = $(element).find('.app-aware-link').attr('href');
+      const companyUrlFound = companyUrl ? companyUrl : "None";
+      comanyLinkedinUrl.push(companyUrlFound);
+      // Create the object and push into node 
+      const companyObject = { company: company, linkedin_url: companyUrlFound };
+      companyData.push(companyObject);
+    });
+  }
+  else{
+    comanyLinkedinUrl.push("None");
+      const companyObject = { company: company, linkedin_url: "None" };
+      companyData.push(companyObject);
+  }
+}
+
+
+const peopleDetail = []
+for (const people of companyList) {
+  if(people == "None"){
+    const personObject = { people_name: "None", role: "None" };
+    peopleDetail.push(personObject);
+    continue
+  }
+  await page.goto(`https://www.linkedin.com/search/results/people/?keywords=${encodeURIComponent(people + " Top Management")}`, { waitUntil: 'domcontentloaded' });
+  const currentUrl = page.url();
+  console.log(`URL after searching for ${people}:`, currentUrl);
+  const searchResultHTML = await page.content();
+  const $ = cheerio.load(searchResultHTML);
+  const results = $('.reusable-search__result-container').eq(1);
+  const n = results.find('span[dir="ltr"]').text().trim();
+  let name = n.split('•')[0].trim();
+  name = name.split("View")[0].trim();
+  const role = results.find('.entity-result__primary-subtitle').text().trim();
+  const personObject = { people_name: name, role: role };
+  peopleDetail.push(personObject);
+}
+
+const companyDetailsAbout = []
+for (const pageUrl of comanyLinkedinUrl) {
+   if(pageUrl == "None"){
+    const aboutObject = { website: "NA", phone: "NA" };
+    companyDetailsAbout.push(aboutObject);
+    continue
+  }
+  await page.goto(`${pageUrl}about/`, { waitUntil: 'domcontentloaded' });
+  const currentUrl = page.url();
+  console.log(`about page after searching for ${pageUrl}about/:`);
+  const searchResultHTML = await page.content();
+  const $ = cheerio.load(searchResultHTML);
+  const results = $('.ember-view').eq(29);
+  const website = results.find('.link-without-visited-state').text().trim();
+  const phone  = results.find('.link-without-visited-state .ember-view').eq(1).attr('href');
+  const aboutObject = { website: website ? website : "NA", phone: phone ? phone : "NA" };
+  companyDetailsAbout.push(aboutObject);
+}
+
+  let data = combinedArrays(companyData, peopleDetail)
+  data = combinedArrays(data, companyDetailsAbout)
+  console.log(data);
+  await csvWriter.writeRecords(data);
+  console.log('CSV file has been written successfully.');
+
+  await browser.close();
+}
+
+const companyList = [];
+fs.createReadStream('company.csv')
+  .pipe(csv())
+  .on('data', (row) => {
+    const companyNames = Object.values(row);
+    companyNames.forEach((name) => {
+      companyList.push(name);
+    });
+  })
+  .on('end', () => {
+    console.log('Company list has been read from CSV:', companyList);
+    if (companyList.length > 0) {
+      scrape(companyList)
+        .then(() => console.log(' completed'))
+        .catch((error) => console.error('Error:', error));
+    } else {
+      console.error('No valid company names f');
+    }
+  });
---
+++ readme.md
@@ -0,0 +1,44 @@
+# Linkedin Web Scraper/Crawler README
+
+## Objective
+
+Assignment Project
+
+## Introduction
+
+This project is a web scraper/crawler built using JavaScript, Node.js,
+and Puppeteer. It takes a list of companies as input and scrapes data
+from LinkedIn to extract information such as company website links,
+LinkedIn page URLs, and contact information of key personnel (CEO,
+CXO, CFO, etc.).
+
+## Technologies Used
+
+- JavaScript
+- Node.js
+- Puppeteer
+
+## Setup Instructions
+
+1. Clone the repository to your local machine.
+2. Ensure you have Node.js installed. You can download it from
+   [here](https://nodejs.org/).
+3. Navigate to the project directory in your terminal.
+4. Run `npm install` to install the project dependencies.
+5. run project using `node index.js` from terminal.
+
+## Project Structure
+
+- `index.js`: Main script file responsible for scraping LinkedIn data.
+- `company.csv`: Input file containing a list of companies.
+- `results.csv`: Output CSV file containing scraped data.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+## Disclaimer
+
+This project is for assignment. The author does not take any
+responsibility for the misuse of this tool. Use it responsibly and
+ensure compliance with all applicable laws and terms of service.
